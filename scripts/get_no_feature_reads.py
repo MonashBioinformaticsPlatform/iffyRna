f582b710a9de8dbeff12949a88e1bdfa15687a34
serine <tskiril@gmail.com>
2018-11-25 18:02:43 +1100

    dereferenced symlinks

--- scripts/get_no_feature_reads.py
+++ scripts/get_no_feature_reads.py
@@ -1,1 +1,60 @@
-/home/kirill/projects/DarcyEllis/RNAseq_mouse_relA_ko.19.07.2018/multi-mappers/scripts/get_reads.py
+#!/usr/bin/env python3
+# -*- coding: iso-8859-15 -*-
+
+import sys
+import os
+import pysam
+
+# featurecounts files produces with -R CORE flag
+f = sys.argv[1]
+
+h = {}
+
+keep = "Unassigned_NoFeatures"
+
+with open(f) as handler:
+    for i in handler:
+        line = i.strip()
+
+        # Each generated file contains four columns:
+        # - read name
+        # - status (assigned or the reason if not assigned)
+        # - number of targets
+        # - target list
+
+        id, t, n, name = line.split("\t")
+
+        if t == keep:
+            if id in h:
+                #sys.exit("ERROR: can't happend %s" % id)
+                print("WARNING: Shouldn't happen for single-end data %s" % id, file=sys.stderr)
+            h[id] = n
+
+print("MSG: Created big hash")
+
+# second arg is you original bam file 
+b = sys.argv[2]
+samfile = pysam.AlignmentFile(b, 'rb')
+
+out_prefix = sys.argv[3]
+#tmp = os.path.basename(b)
+#tmp2 = tmp.split(".bam")[0]
+#tmp2 = b.split(".bam")[0]
+outfile = out_prefix + "_NoFeatures.bam"
+outfile = pysam.AlignmentFile(outfile, "wb", template=samfile)
+
+cnt1 = 0
+cnt2 = 0
+threshold = 100000
+
+for i in samfile:
+    if i.query_name in h:
+        outfile.write(i)
+        cnt2 += 1
+
+    cnt1 += 1
+
+    if cnt2 > threshold:
+        print("MSG: Processed %s reads" % cnt1)
+        print("MSG: NoFeature reads so far %s " % cnt1)
+        threshold += cnt2
--- scripts/parse_blast.py
+++ scripts/parse_blast.py
@@ -1,1 +1,56 @@
-/home/kirill/projects/DavidMccarthy/Campylobacter_comparative_genomics.2018.07.31/scripts/parse_blast.py
+#!/usr/bin/env python3
+# -*- coding: iso-8859-15 -*-
+
+import sys
+import json
+
+#blast json file
+f = sys.argv[1]
+
+# ok, so I think the structure is such
+# each "report" holds info for a specific query
+# that holds several numbers of hits.
+# I need to get first (top?) hit from each report tag
+# number of report should equal to number of queries
+# each "report", will have "results" section
+
+#search": {
+#          "query_id": "Query_1",
+#          "query_title": "chk1_product_1 Contig_65_159.103:filter(unmasked)|start|1230|length|457",
+#          "query_len": 457,
+#          "hits": [
+#            {
+#              "num": 1,
+#              "description": [
+#                {
+#                  "id": "gi|744815412|gb|CP007774.1|",
+#                  "accession": "CP007774",
+#                  "title": "Campylobacter volucris LMG 24379, complete genome",
+#                  "taxid": 580033,
+#                  "sciname": "Campylobacter volucris LMG 24379"
+#                }
+#              ],
+
+print('\t'.join(("Query", "Hit", "Taxid", "Accession", "E.value", "Score")))
+with open(f) as handler:
+    parsed_json = json.loads(handler.read())
+    blast = parsed_json["BlastOutput2"]
+    for report in blast:
+        here = report['report']['results']['search']
+        query = here["query_title"]
+
+        first_hit = here["hits"][0]
+        n = first_hit['num']
+
+        base = first_hit['description'][0]
+        name = base["title"]
+        taxid = str(base["taxid"])
+        id = base["accession"]
+
+        base2 = first_hit['hsps'][0]
+        eval = str(base2['evalue'])
+        score = str(base2['score'])
+
+        print("\t".join((query, name, taxid, id, eval, score)))
+        
+        #print(report['report'].keys())
